4a2ff5d00c224d7b44dca32bb33b65c34cdb38bf
zhujian <505169307@qq.com>
2020-04-17 10:18:36 +0800

    perf(model): 在BasicConv2d中指定激活函数的使用

--- py/lib/models/basic_conv2d.py
+++ py/lib/models/basic_conv2d.py
@@ -8,7 +8,6 @@
 """
 
 import torch.nn as nn
-import torch.nn.functional as F
 
 
 class BasicConv2d(nn.Module):
@@ -16,15 +15,19 @@ class BasicConv2d(nn.Module):
     结合BN的卷积操作
     """
 
-    def __init__(self, in_channels, out_channels, kernel_size=1, stride=1, padding=0, norm_layer=None):
+    def __init__(self, in_channels, out_channels, kernel_size=1, stride=1, padding=0,
+                 norm_layer=None, relu_layer=None):
         super(BasicConv2d, self).__init__()
         if norm_layer is None:
             norm_layer = nn.BatchNorm2d
+        if relu_layer is None:
+            relu_layer = nn.RReLU
 
         self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride=stride, padding=padding)
         self.bn = norm_layer(out_channels, eps=0.001)
+        self.relu = relu_layer(lower=0.1, upper=0.1, inplace=True)
 
     def forward(self, x):
         x = self.conv(x)
         x = self.bn(x)
-        return F.relu(x, inplace=True)
+        return self.relu(x)
